ffbbdb31d49ea08518285546092681897d657ddb
shuvo2712 <shuvo2712@gmail.com>
2025-08-23 15:45:07 +0600

    add-money btn working

--- home.js
+++ home.js
@@ -1,1 +1,21 @@
-console.log('homepage.js connected');
+console.log("homepage.js connected");
+
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log("add money btn clicked");
+    const bank = document.getElementById("input-bank").value;
+    const account = document.getElementById("input-account").value;
+    const ammount = parseInt(document.getElementById("input-ammount").value);
+    const pin = document.getElementById("input-pin").value;
+
+    const availableBalance = parseInt(
+      document.getElementById("main-balance").innerText
+    );
+      console.log("available-balance :", availableBalance);
+      const newBalance = ammount + availableBalance;
+      document.getElementById("main-balance").innerText = newBalance;
+      console.log('ammount-added :', ammount);
+      console.log('new-balance :', newBalance);
+  });
